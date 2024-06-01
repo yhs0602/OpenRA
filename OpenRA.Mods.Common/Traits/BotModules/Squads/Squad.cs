8b834a4cdc0ce644032456db9130b5c8945d9f9b
yhs0602 <jourhyang123@gmail.com>
2024-06-01 14:29:35 +0900

    :bug: Fix save npe

--- OpenRA.Mods.Common/Traits/BotModules/Squads/Squad.cs
+++ OpenRA.Mods.Common/Traits/BotModules/Squads/Squad.cs
@@ -141,7 +141,7 @@ namespace OpenRA.Mods.Common.Traits.BotModules.Squads
 				new("Units", FieldSaver.FormatValue(Units.Select(a => a.ActorID).ToArray()))
 			};
 
-			if (Target != Target.Invalid)
+			if (Target != Target.Invalid && TargetActor != null)
 			{
 				nodes.Add(new MiniYamlNode("ActorToTarget", FieldSaver.FormatValue(TargetActor.ActorID)));
 				nodes.Add(new MiniYamlNode("TargetOffset", FieldSaver.FormatValue(Target.CenterPosition - TargetActor.CenterPosition)));
